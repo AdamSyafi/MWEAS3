0e58782c2433587b8b5518a7a3f40d345d87895f
PBJimm <20ftt1813@student.pb.edu.bn>
2022-05-13 13:20:10 +0800

    small update on order

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,6 +24,14 @@ const routes: Routes = [
     loadChildren: () => import('./order/order.module').then( m => m.OrderPageModule)
   },
   {
+    path: 'manage',
+    loadChildren: () => import('./order/manage/manage.module').then( m => m.ManagePageModule)
+  },
+  {
+    path: 'payment',
+    loadChildren: () => import('./order/manage/payment/payment.module').then( m => m.PaymentPageModule)
+  },
+  {
     path: 'profile',
     loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
   },
@@ -31,6 +39,8 @@ const routes: Routes = [
     path: 'about-us',
     loadChildren: () => import('./about-us/about-us.module').then( m => m.AboutUsPageModule)
   },
+
+
 ];
 
 @NgModule({
